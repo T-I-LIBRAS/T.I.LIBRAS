8956b8d927eff073c0f2919dbc65aad7d26bf68c
edunascc <alexandre.eduardo@escolar.ifrn.edu.br>
2026-09-25 09:35:01 -0300

    .

--- js/supabase-config.js
+++
@@ -1,3 +0,0 @@
-const SUPABASE_URL = 'https://aqponngxttimilzqrcps.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_tjrSjT6iyVjZOh8YMYeAcA_IjmkZnPP';
-window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
